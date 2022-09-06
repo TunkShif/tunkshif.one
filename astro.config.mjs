30ea0025a3bc7a3ea9c318ae593c1203545aaa9a
TunkShif <tunkshif@foxmail.com>
2022-09-06 16:00:52 +0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-
 import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
 import remarkMath from "remark-math"
 import rehypeKatex from "rehype-katex"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tunkshif.one",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
