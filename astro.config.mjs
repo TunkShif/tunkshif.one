9f761b15d65c4e1ca9108ba4ca884e9c3e725a97
TunkShif <tunkshif@foxmail.com>
2022-11-24 21:17:40 +0800

    refactor!: changed react to solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,24 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
-
-import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import solidJs from "@astrojs/solid-js"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import remarkMath from "remark-math"
+import rehypeKatex from "rehype-katex"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tunkshif.one",
-  integrations: [react(), tailwind(), sitemap(), mdx()],
+  integrations: [tailwind(), sitemap(), mdx(), solidJs()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
     extendDefaultPlugins: true,
     syntaxHighlight: "prism"
+  },
+  vite: {
+    ssr: {
+      noExternal: ["solid-headless", "solid-use"]
+    }
   }
-});
+})
--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,67 +1,60 @@
-import React, { useEffect, useState } from "react"
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+import { Portal } from "solid-js/web"
+import { Component, createSignal, For, Show } from "solid-js"
+import ThemeToggle from "./ThemeToggle"
 
-const links = [
-  { text: "Home", route: "/" },
-  { text: "Posts", route: "/posts" },
-  { text: "About", route: "/about" }
-]
-
-const MenuIcon = (
+const BarsIcon = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-    className="w-7 h-7 md:w-6 md:h-6 text-gray-500 dark:text-gray-100 opacity-80 hover:opacity-100"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="w-6 h-6"
   >
     <path
-      fillRule="evenodd"
-      d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z"
-      clipRule="evenodd"
-    ></path>
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    />
   </svg>
 )
 
-const Button = (
-  <button
-    aria-label="Menu"
-    className="ml-2 inline-flex md:hidden justify-center items-center"
-  >
-    {MenuIcon}
-  </button>
-)
-
-export default function NavMenu() {
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    if (import.meta.env.SSR) return
-    setMounted(true)
-  }, [])
-
-  if (!mounted) return Button
+const NavMenu: Component<{ links: { text: string; route: string }[] }> = props => {
+  const [isExpanded, setIsExpanded] = createSignal(true)
 
   return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>{Button}</DropdownMenu.Trigger>
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content
-          align="end"
-          sideOffset={10}
-          className="w-24 rounded-sm px-2 py-1 shadow-sm bg-white dark:bg-gray-800"
-        >
-          {links.map((link) => (
-            <DropdownMenu.Item
-              key={link.route}
-              className="outline-none rounded-sm focus:bg-gray-100 dark:focus:bg-gray-900"
-            >
-              <a href={link.route} className="p-1.5 block w-full font-medium">
-                {link.text}
-              </a>
-            </DropdownMenu.Item>
-          ))}
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
+    <>
+      <button
+        onClick={() => setIsExpanded(!isExpanded())}
+        class="flex justify-center items-center w-8 h-8 text-slate-500 hover:text-slate-600 dark:text-slate-300 dark:hover:text-slate-200"
+      >
+        {BarsIcon}
+      </button>
+      <Portal mount={document.getElementById("navbar-wrapper")!}>
+        <Show when={isExpanded()}>
+          <div class="mx-4 mt-4">
+            <ul class="flex justify-between items-center space-x-8">
+              <For each={props.links}>
+                {item => (
+                  <li>
+                    <a class="font-medium" href={item.route}>
+                      {item.text}
+                    </a>
+                  </li>
+                )}
+              </For>
+            </ul>
+            <div class="mt-4 pt-4 border-t border-slate-200 dark:border-slate-200/10">
+              <div class="flex justify-between items-center">
+                <span class="font-medium">Switch Theme</span>
+                <ThemeToggle />
+              </div>
+            </div>
+          </div>
+        </Show>
+      </Portal>
+    </>
   )
 }
+
+export default NavMenu
