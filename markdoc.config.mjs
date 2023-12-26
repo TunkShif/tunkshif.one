5a63458581b682435f6aa9e4b7220ee35ae56d3f
TunkShif <tunkshif@foxmail.com>
2023-12-26 20:27:48 +0800

    feat: code playground

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,7 +1,25 @@
-import { defineMarkdocConfig } from "@astrojs/markdoc/config"
+import { component, defineMarkdocConfig } from "@astrojs/markdoc/config"
 import shiki from "@astrojs/markdoc/shiki"
 
+const tags = {
+  playground: {
+    render: component("./src/components/playground.astro"),
+    attributes: {
+      template: {
+        type: String
+      },
+      files: {
+        type: Array
+      },
+      dependencies: {
+        type: Array
+      }
+    }
+  }
+}
+
 export default defineMarkdocConfig({
+  tags,
   extends: [
     shiki({
       theme: "one-dark-pro",
